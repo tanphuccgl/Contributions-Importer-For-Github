f90995e0ae5ddd519504c13c3c36d44912157923
Levi <phamnguyentanphucgl@gmail.com>
2023-12-14 11:52:45 +0700

    first commit

---
+++ run_script.py
@@ -0,0 +1,13 @@
+# Contributions-Importer-For-Github/run_script.py
+import git
+from git_contributions_importer import *
+
+# Your private repo or Bitbucket repo
+repo = git.Repo("/Users/levi2k/Desktop/goflydrone")
+# Your mock repo
+mock_repo = git.Repo("/Users/levi2k/Desktop/Bitbucket-Contributions")
+importer = Importer([repo], mock_repo)
+# I use both my personal email and work email here,
+# Since the private repo uses work email, and Github uses my personal email
+importer.set_author(['phamnguyentanphucgl@gmail.com', 'tanphuc@cooldev.vn',])
+importer.import_repository()
